fee3fe20c1f858bb01a39b1887d8e9b106d2e13e
Ksenia Smith <smithkseniav@gmail.com>
2015-03-30 12:04:39 -0700

    basic html and style

---
+++ js/controller.js
@@ -0,0 +1,36 @@
+angular
+	.module("tttApp")
+	.controller("MainController", MainController);
+
+	function MainController () {
+		var self = this;
+
+		//Revise this:
+		self.squares = [
+			{sq: false,
+			 value: null},
+			{sq: false,
+			 value: null},
+			{sq: false,
+			 value: null},
+			{sq: false,
+			 value: null},
+			{sq: false,
+			 value: null},
+			{sq: false,
+			 value: null},
+			{sq: false,
+			 value: null},
+			{sq: false,
+			 value: null},
+			{sq: false,
+			 value: null}
+		];
+
+
+
+
+
+
+
+	} // MainController END
